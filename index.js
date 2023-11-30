dc464fc0330794bbf4c01dad1458aeaa3d6f4fa8
Louis-maucourt <louis.maucourt7@gmail.com>
2023-11-30 13:50:19 +0100

    exercice 1

--- index.js
+++ index.js
@@ -30,3 +30,28 @@ const tweets = [
     likes: 20
   }
 ]
+
+const tweetsList = document.getElementById('tweets-list')
+const content = document.createElement('p')
+const likes = document.createElement('p')
+const author = document.createElement('p')
+const tweet = document.createElement('li')
+
+//console.log(tweetsList,content, likes, author)
+
+tweetsList.classList.add('tweet')
+tweet.classList.add('tweet')
+content.classList.add('content')
+likes.classList.add('likes')
+author.classList.add('author')
+
+content.textContent = tweets[0].content
+likes.textContent = tweets[0].likes
+author.textContent = tweets[0].author
+
+tweetsList.prepend(tweet)
+tweet.prepend(content)
+tweet.prepend(likes)
+tweet.append(author)
+
+
